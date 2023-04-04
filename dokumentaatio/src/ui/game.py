730a6c604a29c9314b58ba70f37d4e942ea40435
Antti_Lumppio <antti.lumppio@helsinki.fi>
2023-04-04 22:28:26 +0300

    viikko3

---
+++ dokumentaatio/src/ui/game.py
@@ -0,0 +1,53 @@
+import random
+
+class Dices:
+    def __init__(self):
+        self.dice = [random.randint(1,6) for i in range(5)]
+        self.roll_amount = 1
+    
+    def roll_dice(self):
+        if self.roll_amount<3:
+            self.dice = [random.randint(1,6) for i in range(5)]
+            self.roll_amount += 1
+        else:
+            return 
+
+
+
+class Player:
+    def __init__(self):
+        self.minutes = {
+            "Ones": None,
+            "Twos": None,
+            "Threes": None,
+            "Fours": None,
+            "Fives": None,
+            "Sixes": None,
+            "Three of a Kind": None,
+            "Four of a Kind": None,
+            "Full House": None,
+            "Small Straight": None,
+            "Large Straight": None,
+            "Yatzy": None,
+            "Chance": None,
+            }
+
+    def Set_Ones(self, dices):
+        if self.minutes['Ones'] is None:
+            sum = 0
+
+            for x in dices.dice:
+                if x == 1:
+                    sum += x
+
+            self.minutes['Ones'] = sum
+
+
+d = Dices()
+p = Player()
+
+print(d.dice)
+print(p.minutes['Ones'])
+
+p.Set_Ones(d)
+print(p.minutes['Ones'])
