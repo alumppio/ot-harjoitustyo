df588d4f07739a77d7132e99d41c2d3e85ffa545
Antti Lumppio <98692578+alumppio@users.noreply.github.com>
2023-05-14 21:47:22 +0300

    Update end_game.py

--- src/config/end_game.py
+++ src/config/end_game.py
@@ -8,9 +8,19 @@ from repositories.delay_time import Delay
 
 class EndGame:
     def __init__(self, players):
+        '''
+        Class to handle the ending of the game. 
+        
+        Args:
+            players (list) : list of Player-class objects
+        '''
         self.players = players
 
     def set_high_scores(self):
+        '''
+        Method to set the high scores of the players in the database
+        '''
+        
         for player in self.players:
             try:
                 CONNECTION.execute(
@@ -24,10 +34,18 @@ class EndGame:
 
 
     def show_high_scores(self):
+        '''
+        Method to draw the high scores on to the screen
+        '''
+        
         drawer = DrawEndGame()
         drawer.draw_high_scores()
 
     def end_game(self):
+        '''
+        Main method to handle the ending of the game. Uses the classes other methods.
+        '''
+        
         self.set_high_scores()
         self.show_high_scores()
         Delay(30000)
