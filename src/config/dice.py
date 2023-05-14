4712526349c964cd65febfb103b8bfe3e5d6bc78
Antti Lumppio <98692578+alumppio@users.noreply.github.com>
2023-05-14 21:44:03 +0300

    Update dice.py

--- src/config/dice.py
+++ src/config/dice.py
@@ -3,10 +3,20 @@ import random
 
 class Dices:
     def __init__(self):
+        ''' 
+        Class that resembles the dices in Yatzy. 5 random integers between 1-6 in a list.
+        '''
+        
         self.dice = [random.randint(1, 6) for i in range(5)]
         self.roll_amount = 1
 
     def roll_dice(self, dices_to_hold):
+        '''
+        Method to roll the dice.
+        
+        Args:
+            dices_to_hold (list) : what dices to hold when rolling.
+        '''
         if self.roll_amount < 3:
             for dice_index in [0, 1, 2, 3, 4]:
                 if dice_index not in dices_to_hold:
