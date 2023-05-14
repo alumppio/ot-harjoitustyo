debfcdac53dbe54bdbad6e9f3fde5096606d2f30
Antti Lumppio <98692578+alumppio@users.noreply.github.com>
2023-05-14 22:01:12 +0300

    Update main_loop.py

--- src/config/main_loop.py
+++ src/config/main_loop.py
@@ -27,17 +27,32 @@ class MainLoop:
         Delay(20000)
 
     def draw_names(self):
+        '''
+        Method to draw the player names on to the screen
+        '''
+        
         for player in self.players:
             player.yatzy_sheet.draw_name(
                 player.player, self.players.index(player))
 
     def check_if_done(self):
+        '''
+        Method to check if the game has been played through
+        '''
+        
         for player in self.players:
             if not player.player.check_total():
                 break
             self.running = False
 
     def event_handle_loop(self, player):
+        '''
+        Loop that handles the events that have occured in the game of a given player
+        
+        Args:
+            player (EventHandler) : EventHandler-class object that contains all the needed information of the player
+        '''
+        
         player.dice_drawer.clean_dice()
         player.current_player()
         while player.running:
