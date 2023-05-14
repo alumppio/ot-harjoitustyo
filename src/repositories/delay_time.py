ae33006e1bfeeac0c0f82bf02b6639c7d398212e
Antti Lumppio <98692578+alumppio@users.noreply.github.com>
2023-05-14 22:51:29 +0300

    Update delay_time.py

--- src/repositories/delay_time.py
+++ src/repositories/delay_time.py
@@ -3,11 +3,28 @@ from repositories.constants import TIMER, FPS
 
 
 class Delay:
+    '''
+    Class to cause the game to pause for a given amount of time
+    '''
+    
     def __init__(self, time):
+        '''
+        Args:
+            time (int) : given amount of time in pygame.ticks
+        '''
+        
         self.running = True
         self.delay_time(time)
 
     def delay_time(self, time):
+        '''
+        Main method to delay the game
+        
+        Args:
+            time (int) : given amount of time in pygame.ticks
+        '''
+        
+        
         while self.running:
             TIMER.tick(FPS)
             pygame.event.get()
